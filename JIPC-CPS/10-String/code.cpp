9a70bf9742710f9afe57e470de73ac1b2f278829
bappasahabapi <bappasaha161@gmail.com>
2025-07-07 01:19:13 +0600

    10. string added

---
+++ JIPC-CPS/10-String/code.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+// #include<iostream>
+// #include<string>
+using namespace std;
+
+// cmd /c "g++ -std=c++11 code.cpp && a.exe
+int main()
+{
+
+     string s = "hello_world";
+    // s.push_back('n');
+    // s.append(" world");
+
+    // cout<<s<<" " <<endl;
+    // for(char c:s) cout<<c<<" ";
+
+    // todo: print character
+    //  for (int i=0; i<s.size(); i++){
+    //      cout<<s[i]<<" ";
+    //  }
+
+    // todo: sorting + reverse
+    // sort(s.begin(), s.end());
+    // cout << s << endl;
+    // reverse(s.begin(), s.end());
+    // cout << s;
+
+
+    //count
+
+    return 0;
+}
