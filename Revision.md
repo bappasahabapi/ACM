924277615919288a139562e86cfd727a82f157be
bappasaha <bappabappa840@gmail.com>
2025-08-31 02:01:03 +0600

    Update

--- Revision.md
+++ Revision.md
@@ -10,7 +10,6 @@
     0^0 =0,
     
 
-
 - **LeetCode-136:** [Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.]
 
 ```cpp
--- code.cpp
+++ code.cpp
@@ -1,30 +1,18 @@
 
 // g++ -std=c++11 code.cpp -o code && ./code
+// g++ -std=c++11 code.cpp && ./a.out 
 #include <iostream>
-#include <vector>
 using namespace std;
-
-int singleNumber(vector<int> &nums)
-{
-    int ans = 0; // use only constant extra space.
-    for (int value : nums)
-    {
-        ans = value ^ ans;
-    }
-
-    // for (int i = 0; i < nums.size(); i++)
-    // {
-    //     ans = ans ^ nums[i];
-    // }
-
-    return ans;
-}
 
 int main()
 {
 
-    vector<int> nums = {1, 1, 3, 5, 4, 5, 4};
-    cout << singleNumber(nums) << endl;
+
+    // int balance = 2000;
+    // if(balance>15000){
+    //     cout<<""
+    // }
+
 
     return 0;
 }
